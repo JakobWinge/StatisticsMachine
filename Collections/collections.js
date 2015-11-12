3e24d7d462e14f49c5cb25d2597fa8007d51d1fa
Jeppe Stougaard <jstougaard@gmail.com>
2015-11-12 12:32:30 +0100

    Added tags input packages

--- Collections/collections.js
+++ Collections/collections.js
@@ -1,6 +1,7 @@
 Images = new Meteor.Collection("images");
 SchoolClasses = new Meteor.Collection("school_classes");
 Sensors = new Meteor.Collection("sensors");
+ImageTags = new Meteor.Collection("image_tags");
 
 function fooSearch(arg1) {
     console.log("fooSearch", arg1);
