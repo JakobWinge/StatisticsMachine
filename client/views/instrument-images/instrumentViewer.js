80b12d91d0a7f00c6bbde3e2e7f3cbba21b5cdb1
Jeppe Stougaard <jstougaard@gmail.com>
2015-12-17 11:15:16 +0100

    Show related

--- client/views/instrument-images/instrumentViewer.js
+++ client/views/instrument-images/instrumentViewer.js
@@ -1,5 +1,8 @@
 Template.instrumentViewer.helpers({
     relatedPictures : function() {
         return Images.find({'refs' : this._id, 'state' : "picture"});
+    },
+    relatedPicturesCount: function() {
+        return Images.count({'refs' : this._id, 'state' : "picture"});
     }
 });
